8575cefbf3d0c8b9cd4e6530781f49ec4ca73ed8
Rafaelx <meow7747@gmail.com>
2023-10-10 22:22:46 +0700

    Create Ison

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 function backHome() {
   window.location.href = "index.html"
 }
+
+let val;
+
+const addBlogs = async () => {
+  await fetch('./data.json').then((res) => res.json()).then((data) => {
+    data.push({ id: 3, title: 'Third Blog' });
+    console.log(data);
+  });
+}
+
+const showBlogs = async () => {
+  await fetch('./data.json').then((res) => res.json()).then((data) => {
+    console.log(data);
+  });
+}
+
+
+showBlogs();
